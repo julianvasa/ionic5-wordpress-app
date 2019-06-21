8179e391bc85d76a76989ffa1feb6a9e7c2f44fe
Julian Vasa <julianvasa@Julians-Air.home.gateway>
2019-06-21 21:00:44 +0200

    eslint vulnerability

--- src/app/services/wordpress.service.ts
+++ src/app/services/wordpress.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -10,34 +10,34 @@ export class WordpressService {
     mainUrl: String = "https://public-api.wordpress.com/rest/v1.1/sites/unegatuaj.com/";
 
     constructor(private http: HttpClient) {
-        if(this.wp_org == true) {
+        if (this.wp_org == true) {
             this.mainUrl = "http://demo.wp-api.org/wp-json/wp/v2/";
         }
     }
-    
+
     public getPosts(page: number): any {
-        return this.http.get(this.mainUrl +"posts/?status=publish&page="+page);
+        return this.http.get(this.mainUrl + "posts/?status=publish&page=" + page);
     }
 
     public getPostsByCat(categoryName: string, page: number): any {
-        if(this.wp_org){
-            return this.http.get(this.mainUrl + "posts/?status=publish&categories="+categoryName+"&page="+page);
+        if (this.wp_org) {
+            return this.http.get(this.mainUrl + "posts/?status=publish&categories=" + categoryName + "&page=" + page);
         }
-        return this.http.get(this.mainUrl + "posts/?status=publish&category="+categoryName+"&page="+page);
+        return this.http.get(this.mainUrl + "posts/?status=publish&category=" + categoryName + "&page=" + page);
     }
 
     public getCategories(): any {
-        if(this.wp_org){
+        if (this.wp_org) {
             return this.http.get(this.mainUrl + "categories?order_by=count&order=desc");
         }
         return this.http.get(this.mainUrl + "categories?order_by=count&order=DESC");
     }
 
     public search(searchStr: string, page: number): any {
-        return this.http.get(this.mainUrl + "posts/?status=publish&search="+searchStr+"&page="+page);
+        return this.http.get(this.mainUrl + "posts/?status=publish&search=" + searchStr + "&page=" + page);
     }
 
     public getPost(recipeId: string): any {
-        return this.http.get(this.mainUrl + "posts/"+recipeId);
+        return this.http.get(this.mainUrl + "posts/" + recipeId);
     }
 }
